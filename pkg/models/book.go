f8be2f8b04954c4cef0867ea057ce77f0a378c2a
Talha Yazar <talhayazarg@gmail.com>
2022-09-18 00:31:34 +0300

    Added global db variable and made initialization for connection with db

--- pkg/models/book.go
+++ pkg/models/book.go
@@ -7,9 +7,18 @@ import (
 	"github.com/talha-yazar/Bookstore-Management-API/pkg/config"
 )
 
+var db *gorm.DB
+
 type Book struct {
 	gorm.Model
 	Name        string `gorm:""json:"name"`
 	Author      string `gorm:""json:"author"`
 	Publication string `gorm:""json:"publication"`
+}
+
+func init() {
+	config.Connect()
+
+	db = config.GetDB()
+	db.AutoMigrate(&Book{})
 }
