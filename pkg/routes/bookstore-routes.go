b6d67faab33772386b13c9b8408f4cdff24d9638
Talha Yazar <talhayazarg@gmail.com>
2022-09-18 15:02:22 +0300

    Changed some naming convention for our routes

--- pkg/routes/bookstore-routes.go
+++ pkg/routes/bookstore-routes.go
@@ -9,6 +9,6 @@ var RegisterBookStoreRoutes = func(router *mux.Router) {
 	router.HandleFunc("/book/", controllers.GetAllBooks).Methods("GET")
 	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
 	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
-	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
-	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
+	router.HandleFunc("/book/{bookId}", controllers.UpdateBookById).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", controllers.DeleteBookById).Methods("DELETE")
 }
